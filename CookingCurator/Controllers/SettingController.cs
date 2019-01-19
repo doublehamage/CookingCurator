8e343526d4c20f9cadab32956010d4665d966a5a
mgquan@myseneca.ca <mgquan@myseneca.ca>
2019-01-19 14:50:53 -0500

    form framework

---
+++ CookingCurator/Controllers/SettingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookingCurator.Controllers
+{
+    public class SettingController : Controller
+    {
+        // GET: Setting
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            ViewBag.Message = "Change Username";
+            ViewBag.Head = "Change Username";
+            ViewBag.Username = "DoubleHamage";
+
+            return View();
+        }
+    }
+}
